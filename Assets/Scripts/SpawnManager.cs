a4d40ca5372101380f5145e5cfa746ac1034b1b6
Montesa3190549 <montesa_3190549@letran-calamba.edu.ph>
2022-10-06 17:55:40 +0800

    AI Traffic
    Implemented AI Traffic movement

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,23 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SpawnManager : MonoBehaviour
 {
+    public NavMeshSurface surface;
+
     RoadSpawner roadSpawner;
     // Start is called before the first frame update
     void Start()
     {
         roadSpawner = GetComponent<RoadSpawner>();
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        surface.BuildNavMesh();
     }
     public void SpawnTriggerEntered(){
         roadSpawner.MoveRoad();
+        
     }
 }
